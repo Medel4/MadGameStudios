49a2c336d32da95ff726475d324c102d27fa0335
Diegolo03 <diego.atila099@gmail.com>
2024-10-28 18:25:21 +0100

    mapa movimiento y personaje

--- Assets/Scripts/FirstPerson.cs
+++ Assets/Scripts/FirstPerson.cs
@@ -26,11 +26,11 @@ public class FirstPerson: MonoBehaviour
         float h = Input.GetAxisRaw("Horizontal");
         float v = Input.GetAxisRaw("Vertical");
         Vector2 input = new Vector2(h, v).normalized;
+        transform.rotation = Quaternion.Euler(0, cam.transform.eulerAngles.y, 0);
 
         if (input.sqrMagnitude > 0)
         {
             float anguloRotacion = Mathf.Atan2(input.x, input.y) * Mathf.Rad2Deg + cam.transform.eulerAngles.y;
-            transform.eulerAngles = new Vector3(0, anguloRotacion, 0);
             Vector3 movimiento = Quaternion.Euler(0, anguloRotacion, 0) * Vector3.forward;
 
             cc.Move(movimiento * velocidadMovimiento * Time.deltaTime);
