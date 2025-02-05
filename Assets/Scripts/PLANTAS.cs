5cb349092a78f8d2ff1e4ee9d072ae9fc5ac2b82
DESKTOP-GV16N45\Alumno <diego.atila099@gmail.com>
2025-02-05 21:19:13 +0100

    arboles revisar fernando

--- Assets/Scripts/PLANTAS.cs
+++ Assets/Scripts/PLANTAS.cs
@@ -9,23 +9,44 @@ public class PLANTAS : MonoBehaviour
     private float tamanoactual;
     [SerializeField]private float tamanoinicial= 0.00001f;
     private float velocidadCrecimiento;
+    [SerializeField] private bool funcionar=false;
     // Start is called before the first frame update
     private void Awake()
     {
         tamanomaximo = 1f;
         velocidadCrecimiento = 1.4F * Time.deltaTime;
         tamanoinicial = 0.0001f;
-        transform.localScale = new Vector3(0, 0, 0);
+        transform.localScale = new Vector3(0.000001f, 0.000001f, 0.000001f);
     }
 
     // Update is called once per frame
     void Update()
     {
-        tamanoactual = transform.localScale.magnitude;
-        if (tamanoactual < tamanomaximo)
-        {
-            transform.localScale += new Vector3(tamanoinicial , tamanoinicial , tamanoinicial) *velocidadCrecimiento;
+        
 
+         if (funcionar==true)
+        {
+            tamanoactual = transform.localScale.magnitude;
+            if (tamanoactual < tamanomaximo)
+            {
+                transform.localScale += new Vector3(tamanoinicial, tamanoinicial, tamanoinicial) * velocidadCrecimiento;
+
+            }
         }
     }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("aire"))
+        {
+            funcionar = true;
+            Debug.Log("puta");
+        }
+        else if (other.CompareTag("volagua"))
+        {
+            Debug.Log("1puta");
+            funcionar = false;
+        }
+         
+    }
+
 }
